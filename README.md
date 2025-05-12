551437087cc184e3dfebc865b3099ee299fccfe2
mirzanahid <mirzasaikat3@gmail.com>
2025-05-12 19:54:53 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# JavaScript Problems
+
+This repository contains a collection of simple JavaScript problems to practice various concepts such as array manipulation, functions, and conditionals.
+
+## Problems
+
+### 1. Array Filtering and Mapping  
+
+- Filter out all females from an array of objects and map the remaining people to an array of names.
+
+### 2. Array Reduction  
+- Calculate the sum of all even numbers in an array using the `reduce` method.
+### 3. Leap Year Checker  
+- Determine whether a given year is a leap year.
+
+### 4. Unique Values  
+- Filter out duplicate values from an array and return an array of unique numbers.
+
+### 5. Find Maximum Value  
+- Write a function to find the maximum value in an array of numbers.
+
+
+
+## How to Run
+
+1. Clone this repository to your local machine.
+2. Open the files in a browser or a code editor to see the JavaScript code.
+3. Run the code in the browser's console or in a Node.js environment to see the results.
+
+
+
+
---
+++ problemSoliving.js
@@ -0,0 +1,57 @@
+//   1. Task: Array Filtering and Mapping
+
+const people = [
+  { name: "Alice", age: 25, gender: "female" },
+  { name: "Bob", age: 30, gender: "male" },
+  { name: "Charlie", age: 35, gender: "male" },
+  { name: "Diana", age: 28, gender: "female" },
+];
+
+function filterAndMapPeople(people) {
+  return people
+    .filter((person) => person.gender !== "female")
+    .map((person) => person.name);
+}
+
+console.log(filterAndMapPeople(people));
+
+// 5. Task: Array Reduction
+
+function sumEvenNumbers(arr) {
+  return arr.reduce((sum, num) => {
+    if (num % 2 === 0) {
+      return sum + num;
+    }
+    return sum;
+  }, 0);
+}
+
+const numberss = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(sumEvenNumbers(numberss));
+
+//   6. Task: Leap Year Checker
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+console.log(isLeapYear(2020));
+console.log(isLeapYear(2021));
+
+//   7. Task: Unique Values
+
+function uniqueNumbers(arr) {
+  return [...new Set(arr)];
+}
+
+const number = [1, 2, 3, 4, 2, 3, 5];
+console.log(uniqueNumbers(number));
+
+//   8. Task: Find Maximum Value
+
+function findMax(arr) {
+  return Math.max(...arr);
+}
+
+const numbers = [5, 10, 2, 8, 3];
+console.log(findMax(numbers));
